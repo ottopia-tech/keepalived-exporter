5c8c0d7dd89727f4e3c4c01e30dc9cd76ddd68be
mohammad <mohammad@ottopia.tech>
2023-08-24 10:44:51 +0300

    update script

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -3,17 +3,18 @@
 set_variables() {
     readonly internet_device=$1
     readonly backup_address=$2
-    readonly gw=$3
-    readonly mode=$6
+    readonly internet_subnet = $3 
+    readonly gw=$4
+    readonly mode=$7
 }
 
 setup_device_ip() {
     if [ "${mode}" == "MASTER" ]
     then
 	ip route del default via ${gw} src ${backup_address} dev ${internet_device}
-        ip address del ${backup_address}/24 dev ${internet_device}
+        ip address del ${backup_address}/${internet_subnet} dev ${internet_device}
     else
-        ip address add ${backup_address}/24 dev ${internet_device}
+        ip address add ${backup_address}/${internet_subnet} dev ${internet_device}
 	ip route add default via ${gw} src ${backup_address} dev ${internet_device}
     fi
 }
